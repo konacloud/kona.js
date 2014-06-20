21ee403d0cac75a1f02daa13c36168f1b99c4437
santiago <santiago@konacloud.io>
2014-06-20 14:42:52 -0300

    bug fix classpath cahnges

--- OpenKonaJS/src/org/kona/js/exceptions/KonaException.java
+++ OpenKonaJS/src/org/kona/js/exceptions/KonaException.java
@@ -2,8 +2,25 @@ package org.kona.js.exceptions;
 
 public class KonaException extends Exception {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 9090450786149913212L;
+	public String msg;
+
 	public KonaException(String msg) {
+
 		super(msg);
+		this.setMsg(msg);
+
+	}
+
+	public String getMsg() {
+		return msg;
+	}
+
+	public void setMsg(String msg) {
+		this.msg = msg;
 	}
 
 }
